2cffa0e7c57194e0951d1ec74c9adade202d8953
firstcreditunion <gateway@firstcu.co.nz>
2022-11-25 12:30:12 +1300

    postgres build error

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+
+"""
+
+def add (x,y):
+  """Adds x and y"""
+  return x+y
+
+def subtract(x,y):
+  """Subratcs two numbers"""
+  return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,27 @@
+"""
+Sample Tests
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+  """
+  test the calc module
+  
+  """ 
+
+  def test_add_numbers(self):
+    """Test Adding Numbers"""
+    res = calc.add(10,20)
+
+    self.assertEqual(res, 30)
+
+  def test_subtract_numbers(self):
+    """Testing suntracting numbers"""
+
+    res = calc.subtract(15,10)
+
+    self.assertEquals(res, -5)
