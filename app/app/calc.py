e17c82e40a25c5e1631ed4187b9e9c5db2cb320a
firstcreditunion <gateway@firstcu.co.nz>
2022-11-25 15:15:51 +1300

    Linting errors fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,10 @@ Calculator functions
 
 """
 
-def add (x,y):
+def add(x, y):
   """Adds x and y"""
   return x+y
 
-def subtract(x,y):
+def subtract(x, y):
   """Subratcs two numbers"""
   return y-x
